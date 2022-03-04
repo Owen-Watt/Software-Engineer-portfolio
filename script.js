15a2e39110dc5d00d7e02a8401f686763d0817b9
ntc729 <ntcminecraft1@gmail.com>
2022-03-04 15:22:52 +0000

    fixed incorrect links on home page minor changes to button designs fixed mobile menu not displaying correctly

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 window.onscroll = (() => {
   var navBar = document.getElementById("navBar");
+  var mobilenavbar = document.getElementById("mobileNavbar");
 
   /* change the height, font colour and shadow of navbar once scrolled */
   if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
     navBar.style.height = "4em";
     navBar.style.boxShadow = "0 3px 5px rgb(0 0 0 / 0.2)";
+    mobilenavbar.style.height = "3em";
+    mobilenavbar.style.boxShadow = "0 3px 5px rgb(0 0 0 / 0.2)";
   } else {
     navBar.style.height = "7em";
     navBar.style.boxShadow = "0 0px 0px rgb(0 0 0 / 0.0)";
+    mobilenavbar.style.height = "4em";
+    mobilenavbar.style.boxShadow = "0 0px 0px rgb(0 0 0 / 0.0)";
   }
 });
 
